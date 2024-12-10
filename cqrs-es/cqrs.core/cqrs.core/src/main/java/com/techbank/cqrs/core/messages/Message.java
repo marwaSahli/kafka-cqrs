9b84542395cffd96ed311b23ed83eeb2201ffcc3
marwaSahli <marwa.sahli.marwa@gmail.com>
2024-12-10 15:28:48 +0100

    first commit

---
+++ cqrs-es/cqrs.core/cqrs.core/src/main/java/com/techbank/cqrs/core/messages/Message.java
@@ -0,0 +1,12 @@
+package com.techbank.cqrs.core.messages;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Message {
+    private String id;
+}
